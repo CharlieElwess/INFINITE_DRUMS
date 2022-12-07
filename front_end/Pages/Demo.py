7c224cadb841ab6991cba5bf2aea5d5e6114069f
Chloe Guiver <chloejguiver@gmail.com>
2022-12-07 11:40:33 +0000

    streamline changes

---
+++ front_end/Pages/Demo.py
@@ -0,0 +1,26 @@
+import streamlit as st
+
+
+
+
+st.markdown(
+  """
+  <style>
+  [class="block-container css-12oz5g7 egzxvld2"]{
+    background-color: rgb(0,100,100);
+  }
+  [class="main css-k1vhr4 egzxvld3"]{
+    background-color: rgb(0,100,100);
+  }
+  [class="css-18ni7ap e8zbici2"]{
+    background-color: rgb(0,100,100);
+  }
+  .sidebar .sidebar-content {
+    background-color: rgb(0,100,100);
+  }
+  </style>
+  """,
+  unsafe_allow_html=True
+)
+
+st.video('https://youtu.be/htwXH6QbAL8')
---
+++ front_end/about_page_design.py
@@ -0,0 +1,74 @@
+
+def get_css2():
+    return '''
+
+    <style>
+
+
+
+    [class="main css-k1vhr4 egzxvld3"]{
+        /* background-image: url("https://live.staticflickr.com/1937/31029969758_d59c78ae0f_b.jpg"); */
+        /* background-image: url("https://upload.wikimedia.org/wikipedia/commons/0/03/Meshuggah_Stage_Design_2022%2C_Tomas_Haake_Drumkit%2C_Ancienne_Belgique%2C_Belgium._13.jpg"); */
+        background-image: url("https://i0.hippopx.com/photos/531/616/16/battery-musical-instruments-music-preview.jpg");
+        background-attachement: fixed;
+        background-size: cover
+    }
+
+    div.css-183lzff.eyqtai90{
+        background-color: white;
+    }
+
+    div.css-1g42dsv.e1s6o5jp0{
+        background-color: white;
+    }
+
+    div.css-m7esqn.epcbefy1{
+        background-color: white;
+    }
+
+    p {
+    color: rgb(255,255,255);
+    font-family: 'Helvetica', monospace;
+    font-size: 25px;
+    -webkit-text-fill-color: rgb(255,255,255);
+    -webkit-text-stroke: 1px;
+    }
+
+    h1 {
+    font-family: 'Menlo', monospace;
+    color: rgb(255,250,250);
+    font-size: 100px;
+    -webkit-text-fill-color: rgb(46,118,94);
+    -webkit-text-stroke: 1px;
+    }
+
+    h2 {
+    color: rgb(255,255,255);
+    -webkit-text-stroke: 1px
+    }
+
+    h3 {
+    color: rgb(255,255,255);
+    font-family: 'Helvetica', monospace;
+    -webkit-text-fill-color: rgb(255,255,255);
+    -webkit-text-stroke: 2px
+    background-color: white;
+    }
+
+
+    ol {
+    color: grey;
+    -webkit-text-stroke: 1px
+    }
+
+    ul {
+    color: rgb(255,255,255);
+    -webkit-text-stroke: 1px
+    }
+
+    .css-nlntq9 a {
+    color: rgb(0,128,128);
+    }
+
+    </style>
+    '''
